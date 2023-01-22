0adca89e30f7f1ff4d96df45e7f6bddb4f7667ff
gyukiss55 <gs.gkiss@gmail.com>
2023-01-22 19:06:49 +0100

    multithread tested

--- MultiThreadDodekafon.cpp
+++ MultiThreadDodekafon.cpp
@@ -14,19 +14,16 @@ MultiThreadDodekafon.cpp
 
 #define MAX_THREADS  DODEKAFON_SIZE
 
-static HANDLE  hRunMutex[MAX_THREADS] = { NULL };
 static HANDLE  hThreads[MAX_THREADS] = { NULL };
 static HANDLE  hTimerMutex;
 
 struct ThreadData {
-	HANDLE* hThreadPtr{nullptr};
 	std::vector<DodekafonRecord> * resultPtr{nullptr};
 	std::size_t startValue;
 	DWORD timeTick{0};
 
 	ThreadData() {};
-	ThreadData(HANDLE* hThreadPtrIn, std::vector<DodekafonRecord>* resultPtrIn, std::size_t startValueIn, DWORD timeTickIn) :
-		hThreadPtr(hThreadPtrIn),
+	ThreadData(std::vector<DodekafonRecord>* resultPtrIn, std::size_t startValueIn, DWORD timeTickIn) :
 		resultPtr(resultPtrIn),
 		startValue (startValueIn),
 		timeTick (timeTickIn)
@@ -36,7 +33,7 @@ struct ThreadData {
 void DodekafonProcess(void * dataPtr)
 {
 	ThreadData* threadDataPtr = (ThreadData*)dataPtr;
-	if (threadDataPtr != nullptr && threadDataPtr->hThreadPtr != nullptr && threadDataPtr->resultPtr != nullptr) {
+	if (threadDataPtr != nullptr && threadDataPtr->resultPtr != nullptr) {
 		DodekafonRecord start(threadDataPtr->startValue);
 		DiffIteratorType diffIterator{ 0,false };
 		for (DodekafonRecord r = start; r.IsThereNext(); r = r.FindNext(diffIterator)) {
@@ -52,17 +49,45 @@ void DodekafonProcess(void * dataPtr)
 }
 
 
-int SimpleDodekafonV1_MultiThread(bool listMode)
+int SimpleDodekafonV1_MultiThread(bool listMode, std::array<DWORD, DODEKAFON_SIZE>& timetickArray)
 {
-	std::vector<DodekafonRecord> resultThreads[MAX_THREADS];
+	std::vector<DodekafonRecord> resultThreads[DODEKAFON_SIZE];
 	std::vector<DodekafonRecord> result;
-	ThreadData threadData[MAX_THREADS];
+	ThreadData threadData[DODEKAFON_SIZE];
 
 	DWORD tt0 = GetTickCount();
 	for (std::size_t i = 0; i < DODEKAFON_SIZE; ++i) {
-		threadData[i] = ThreadData(&hRunMutex[i], &resultThreads[i], i + 1, tt0);
-		hRunMutex[i] = (HANDLE)CreateMutexW(NULL, TRUE, NULL);
+		threadData[i] = ThreadData(&resultThreads[i], i + 1, tt0);
 		hThreads[i] = (HANDLE)_beginthread(DodekafonProcess, 0, (void*)(ThreadData *)&threadData[i]);
+	}
+	for (std::size_t i = 0; i < DODEKAFON_SIZE; ++i) {
+		WaitForSingleObject(hThreads[i], INFINITE);
+		for (const DodekafonRecord& r : resultThreads[i]) {
+			result.push_back(r);
+		}
+		timetickArray[i] += threadData[i].timeTick;
+	}
+	DWORD tt1 = GetTickCount();
+	if (listMode) {
+		for (const DodekafonRecord& r : result) {
+			r.OutputDebugString("Valid record - ", DodekafonRecord::RecordMode);
+		}
+#ifdef _DEBUG_OUTPUTSTR_
+		std::string s;
+		s = "Found records:";
+		s += std::to_string(result.size ());
+		s += "\n Ellapsed mSec:";
+		s += std::to_string(tt1 - tt0);
+		s += "\n";
+		::OutputDebugStringA(s.c_str());
+		s = "TimeTick:";
+		for (DWORD tt : timetickArray) {
+			s += std::to_string(tt);
+			s += ", ";
+		}
+		s += "\n";
+		::OutputDebugStringA(s.c_str());
+#endif
 	}
 	return result.size();
 }
@@ -70,11 +95,12 @@ int SimpleDodekafonV1_MultiThread(bool listMode)
 
 int MultiThreadDodekafonV1()
 {
+	std::array<DWORD, DODEKAFON_SIZE> timetickArray{ 0 };
 	DWORD tc = GetTickCount();
 	int ret = 0;
 	const int num = 10;
 	for (int i = 0; i < num; ++i) {
-		ret = SimpleDodekafonV1_MultiThread(i == 0);
+		ret = SimpleDodekafonV1_MultiThread(i == 0, timetickArray);
 	}
 	DWORD tc2 = GetTickCount();
 #ifdef _DEBUG_OUTPUTSTR_
